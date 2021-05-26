1d45f91118327bf88a94755ab38e218067c9f47c
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-05-26 15:11:21 +0530

    Add files via upload

---
+++ Diffvalue.java
@@ -0,0 +1,11 @@
+public class Diffvalue
+{
+	public static void main(String[] args)
+	{
+	    int a=020;
+	    int b=0X20;
+	    System.out.println("value of a is " +a);
+		System.out.println("value of b is " +b);
+	}
+}
+
